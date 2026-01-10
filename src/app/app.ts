455f75c82d7e85022b5f0cc61e473636409f642d
CellyMaciel <cellymaciel005@gmail.com>
2026-01-10 20:52:03 -0300

    :sparkles: :: Footer Started

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
 import { Home } from "./component/home/home";
 import { Midle } from "./component/midle/midle";
+import { Footer } from "./component/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, Midle],
+  imports: [Header, Home, Midle, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
